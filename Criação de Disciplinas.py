cf44501ffe7e94289bbd06b8866e4f051942d67e
Lucas Domingues <62187440+Lucasdmg@users.noreply.github.com>
2022-07-11 16:48:48 -0300

    Create Criação de Disciplinas.py

---
+++ Criação de Disciplinas.py
@@ -0,0 +1,56 @@
+import pyautogui as pg
+import time
+print("1.")
+time.sleep(1)
+print("2..")
+time.sleep(1)
+print("3...")
+time.sleep(1)
+print("4....")
+time.sleep(1)
+print("5.....")
+time.sleep(1)
+
+print(pg.position())
+for i in range(1):
+    time.sleep(1)
+    pg.click(x=131, y=13)   # aba nº1
+    time.sleep(1)
+    pg.click(x=299, y=276)  # Click no botao funções
+    time.sleep(1)
+    pg.click(x=303, y=480)  # Click Botão Copiar
+    time.sleep(1)
+    pg.click(x=909, y=617)  # Click em Copiar
+
+
+    pg.click(x=304, y=7)    # aba nº2
+    time.sleep(1)
+    pg.click(x=299, y=276)  # Click no botao funções
+    time.sleep(1)
+    pg.click(x=303, y=480)  # Click Botão Copiar
+    time.sleep(1)
+    pg.click(x=909, y=617)  # Click em Copiar
+
+    pg.click(x=477, y=13)  # aba nº3
+    time.sleep(1)
+    pg.click(x=299, y=276)  # Click no botao funções
+    time.sleep(1)
+    pg.click(x=303, y=480)  # Click Botão Copiar
+    time.sleep(1)
+    pg.click(x=909, y=617)  # Click em Copiar
+
+    pg.click(x=652, y=13)  # aba nº4
+    time.sleep(1)
+    pg.click(x=299, y=276)  # Click no botao funções
+    time.sleep(1)
+    pg.click(x=303, y=480)  # Click Botão Copiar
+    time.sleep(1)
+    pg.click(x=909, y=617)  # Click em Copiar
+
+    pg.click(x=827, y=13)  # aba nº5
+    time.sleep(1)
+    pg.click(x=299, y=276)  # Click no botao funções
+    time.sleep(1)
+    pg.click(x=303, y=480)  # Click Botão Copiar
+    time.sleep(1)
+    pg.click(x=909, y=617)  # Click em Copiar
